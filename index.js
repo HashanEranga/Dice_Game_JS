ab69746787a9b027d760077b9d894c585ca4a665
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2021-08-11 01:48:57 +0530

    build(Button): Added a button for the development

--- index.js
+++ index.js
@@ -32,22 +32,28 @@ function randomGenerator(randomNumber){
     return numberStr;
 }
 
-var listOfPlayers = document.getElementsByClassName('image');
+function handleClick(){
+    var listOfPlayers = document.getElementsByClassName('image');
 
-var number1 = randomizer();
-imageNum1 = randomGenerator(number1);
-listOfPlayers[0].setAttribute('src', 'Assets/'+imageNum1+'.png')
+    var number1 = randomizer();
+    imageNum1 = randomGenerator(number1);
+    listOfPlayers[0].setAttribute('src', 'Assets/'+imageNum1+'.png')
 
-var number2 = randomizer();
-imageNum2 = randomGenerator(number2);
-listOfPlayers[1].setAttribute('src', 'Assets/'+imageNum2+'.png')
+    var number2 = randomizer();
+    imageNum2 = randomGenerator(number2);
+    listOfPlayers[1].setAttribute('src', 'Assets/'+imageNum2+'.png')
 
-if (number1 > number2){
-    document.getElementsByClassName('title')[0].innerHTML = 'Player 01 Wins';
+    if (number1 > number2){
+        document.getElementsByClassName('title')[0].innerHTML = 'Player 01 Wins';
+    }
+    else if(number1 < number2){
+        document.getElementsByClassName('title')[0].innerHTML = 'Player 02 Wins';
+    }
+    else{
+        document.getElementsByClassName('title')[0].innerHTML = 'Draw';
+    }
+
 }
-else if(number1 < number2){
-    document.getElementsByClassName('title')[0].innerHTML = 'Player 02 Wins';
-}
-else{
-    document.getElementsByClassName('title')[0].innerHTML = 'Draw';
-}
+
+
+document.getElementById('buton').addEventListener('click', handleClick);
